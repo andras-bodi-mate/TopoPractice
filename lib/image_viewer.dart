2e3406d8f214d11d973f5c565ee5ea2927ccf575
andras-bodi-mate <bodimate.andras@gmail.com>
2025-04-13 21:54:16 +0200

    Made ImageViewer not constrained to available space.

--- lib/image_viewer.dart
+++ lib/image_viewer.dart
@@ -15,25 +15,58 @@ class ImageViewer extends StatefulWidget {
 }
 
 class ImageViewerState extends State<ImageViewer> {
-  late final TransformationController myTransform;
+  late final TransformationController transformController;
+  late double imageWidth, imageHeight;
 
   @override
   void initState() {
     super.initState();
-    myTransform = widget.controller ?? TransformationController();
+    transformController = widget.controller ?? TransformationController();
+
+    final stream = widget.imageProvider.resolve(const ImageConfiguration());
+    stream.addListener(ImageStreamListener((info, _) {
+      setState(() {
+        imageWidth = info.image.width.toDouble();
+        imageHeight = info.image.height.toDouble();
+      });
+
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        final RenderBox renderBox = context.findRenderObject() as RenderBox;
+        final Size containerSize = renderBox.size;
+
+        final double scaleX = containerSize.width / imageWidth;
+        final double scaleY = containerSize.height / imageHeight;
+        final double scale = scaleX < scaleY ? scaleX : scaleY;
+
+        final double dx = (containerSize.width - imageWidth * scale) / 2;
+        final double dy = (containerSize.height - imageHeight * scale) / 2;
+
+        transformController.value = Matrix4.identity()
+          ..translate(dx, dy)
+          ..scale(scale);
+      });
+    }));
   }
 
-  TransformationController get controller => myTransform;
+  TransformationController get controller => transformController;
 
   @override
   Widget build(BuildContext context) {
     return InteractiveViewer(
-      transformationController: myTransform,
+      transformationController: transformController,
       panEnabled: true,
       scaleEnabled: true,
+      minScale: 1.0,
       maxScale: 15,
+      boundaryMargin: EdgeInsets.all(double.infinity),
       constrained: false,
-      child: Image(image: widget.imageProvider, fit: BoxFit.scaleDown),
+      
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: SizedBox(
+          child: Image(image: widget.imageProvider)
+        )
+      )
     );
   }
 }
